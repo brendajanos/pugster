518ccc9839ff372f675243a52cf9982736c14a69
brendajanos <73362325+brendajanos@users.noreply.github.com>
2021-09-20 19:02:07 +0200

    websupport for react native map

---
+++ packages/frontend/src/App.tsx
@@ -0,0 +1,20 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>HeloBelo{process.env.MAPBOX_TOKEN}</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
